6258dd79ec0ef1d0300ebe3b31931f8d44620091
Mohan <ankitcity18@gmail.com>
2025-12-14 15:24:09 +0530

    initial commit

--- app2.py
+++ app2.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pickle
 import string
 import nltk
+
+nltk.data.path.append("./nltk_data")
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 ps=PorterStemmer()
